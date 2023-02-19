ff1a54d5a1fc807389ba4dbb8549af563aec6541
kenketsu <razgriz.memai02723@gmail.com>
2023-02-19 17:58:43 +0900

    add user list api view

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path("create/", views.CreateUserAPIView.as_view(), name="create"),
+    path("users", views.UserListAPIView.as_view(), name="users"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,16 @@
-from rest_framework import permissions, generics
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
 
 from .serializers import UserSerializer
+
+User = get_user_model()
 
 
 class CreateUserAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
