c8bbdc92d0c92b5cecb1d86b964812f976ce661b
kenketsu <razgriz.memai02723@gmail.com>
2023-03-24 20:31:00 +0900

    update task serializer and add comment

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -10,11 +10,14 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    category_name = serializers.ReadOnlyField(source="category.name")
-    owner_username = serializers.ReadOnlyField(source="owner.username")
-    status_name = serializers.CharField(source="get_status_display", read_only=True)
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
-    total_time = serializers.DurationField(format="%H:%M:%S")
+    category_name = serializers.ReadOnlyField(source="category.name")  # カテゴリー名の文字列
+    owner_username = serializers.ReadOnlyField(source="owner.username")  # タスクのオーナー名の文字列
+    status_name = serializers.CharField(
+        source="get_status_display", read_only=True
+    )  # ステータスの文字列
+    created_at = serializers.DateTimeField(
+        format="%Y-%m-%d %H:%M", read_only=True
+    )  # 作成日時のフォーマット
 
     class Meta:
         model = Task
